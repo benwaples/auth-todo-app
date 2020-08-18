1e891d10823a0ebfbb237e3b169f23f929c45eca
bwapes <benwaples@gmail.com>
2020-08-18 14:16:21 -0700

    tables successfully spun up

---
+++ data/toDos.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    todo: 'wash your feet',
+    completed: false
+  },
+  {
+    todo: 'wash your hair',
+    completed: false
+  },
+  {
+    todo: 'take out the trash',
+    completed: false
+  },
+  {
+    todo: 'wash the fish',
+    completed: false
+  },
+];
